7d3ce844296bc98594de0181908e2414218de885
Thiago Delgado Pinto <thiago_dp@yahoo.com.br>
2026-04-13 19:44:06 -0300

    Checking CI with localhost (mac/linux)

--- spec/server/shared.php
+++ spec/server/shared.php
@@ -1,4 +1,5 @@
 <?php
 const NOT_ALLOWED_ORIGIN = 'http://different-domain.com';
 
-$host = PHP_OS_FAMILY === 'Windows' ? 'localhost' : '127.0.0.1';
+// $host = PHP_OS_FAMILY === 'Windows' ? 'localhost' : '127.0.0.1';
+$host = 'localhost';
